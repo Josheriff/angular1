5772bf6b7f99dc4605243c34dab057f18b906315
josheriff <jakermater@gmail.com>
2017-10-08 12:55:36 +0200

    lesson, in directives use simple words not camelcase, not --- case

--- directives/topic-card.js
+++ directives/topic-card.js
@@ -2,5 +2,11 @@ angular.module("appArg")
 	.directive("topicCard",[topicCard]);
 
 function topicCard(){
-	return { templateUrl: 'views/directives/topic-card.html' };
+	return { 
+		templateUrl: 'views/directives/topic-card.html', 	
+		scope: { data:'=data' },
+		link: function(scope,elem,args){
+			console.log(scope,'<<<<<<<<<');
+		}
+	};
 }
